2d548fb2522098ffa3933e4cd47973eb91d2fbdf
jump <jmperkinsbiz@gmail.com>
2021-06-08 14:37:38 -0700

    create component AudioPlayer

---
+++ ui/src/components/AudioPlayer.vue
@@ -0,0 +1,16 @@
+<template>
+  <audio controls :src="url"></audio>
+</template>
+
+<script>
+export default {
+  name: 'AudioPlayer',
+
+  props: {
+    url: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
