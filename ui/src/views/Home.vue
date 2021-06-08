233d7d6dbfd4c2500b41c22ce0a48016d5ec0eb0
jump <jmperkinsbiz@gmail.com>
2021-06-08 14:47:22 -0700

    POC for capturing audio and playing it back

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="startRecording">Record</button>
+    <button @click="onStopClick">Stop</button>
+
+    <div class="recording-list">
+      <audio-player
+        v-for="(audioURL, i) in audioURLs"
+        :key="i"
+        :url="audioURL"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AudioPlayer from '@/components/AudioPlayer'
 
 export default {
   name: 'Home',
+
+  data () {
+    return {
+      mediaRecorder: undefined,
+      audioCtx: undefined,
+      chunks: [],
+      audioURLs: []
+    }
+  },
+
+  mounted () {
+    if (navigator.mediaDevices.getUserMedia) {
+      console.log('getUserMedia supported')
+
+      const constraints = { audio: true }
+
+      const onError = err => console.error(err)
+
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then(this.setupMediaRecorder, onError)
+    } else {
+      alert('getUserMedia not supported on your browser!')
+    }
+  },
+
+  methods: {
+    setupMediaRecorder (stream) {
+      this.mediaRecorder = new MediaRecorder(stream)
+
+      this.mediaRecorder.onstop = this.stopRecording
+
+      this.mediaRecorder.ondataavailable = e =>
+        this.chunks.push(e.data)
+    },
+
+    startRecording () {
+      this.mediaRecorder.start()
+    },
+
+    stopRecording () {
+      const blob = new Blob(this.chunks, { type: 'audio/ogg; codecs=opus' })
+      this.chunks = []
+      const audioURL = window.URL.createObjectURL(blob)
+      this.audioURLs.push(audioURL)
+    },
+
+    onStopClick () {
+      this.mediaRecorder.stop()
+    }
+  },
+
   components: {
-    HelloWorld
+    AudioPlayer
   }
 }
 </script>
+
+<style scoped lang="scss">
+.recording-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > audio {
+    width: 100%;
+    max-width: 700px;
+    margin-bottom: 16px;
+  }
+}
+</style>
